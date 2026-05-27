3bab8136124a0f576225da2ea20d3fbb933f8364
Hamid-Khosropanahi <Hamid.khp@hotmail.com>
2026-05-28 01:50:24 +0330

    Fix Migration

--- Kharbarchi.Server/Migrations/20260527221253_RenameUserToCustomer.cs
+++ Kharbarchi.Server/Migrations/20260527221253_RenameUserToCustomer.cs
@@ -12,27 +12,29 @@ namespace Kharbarchi.Server.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.UpdateData(
+                  schema: "sec", // using your schema name from the error
+                  table: "AspNetRoles",
+                  keyColumn: "Id",
+                  keyValue: "6240e185-5c3a-410b-99d3-9767571fdf24",
+                  columns: new[] { "Name", "NormalizedName" },
+                  values: new object[] { "Customer", "CUSTOMER" });
+        
             migrationBuilder.InsertData(
-                schema: "sec",
-                table: "AspNetRoles",
-                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[,]
-                {
-                    { "6240e185-5c3a-410b-99d3-9767571fdf24", null, "Customer", "CUSTOMER" },
-                    { "9a4918f4-604a-4e2b-be24-5d5d852ec6a2", "e98e4f5a-5bc4-4845-8b38-dbb4eb5b7964", "AdminSeller", "ADMINSELLER" },
-                    { "db563459-d897-400f-87d2-747d86f2b236", "a71f00a4-3221-4d32-8418-df8db3748c0f", "Seller", "SELLER" },
-                    { "f517b79d-1fc4-4800-bcb8-ee0ca67dce1e", null, "Admin", "ADMIN" }
-                });
+                  schema: "sec",
+                  table: "AspNetRoles",
+                  columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                  values: new object[,]
+                  {
+                      { "9a4918f4-604a-4e2b-be24-5d5d852ec6a2", "e98e4f5a-5bc4-4845-8b38-dbb4eb5b7964", "AdminSeller", "ADMINSELLER" },
+                      { "db563459-d897-400f-87d2-747d86f2b236", "a71f00a4-3221-4d32-8418-df8db3748c0f", "Seller", "SELLER" },
+                  });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DeleteData(
-                schema: "sec",
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "6240e185-5c3a-410b-99d3-9767571fdf24");
+          
 
             migrationBuilder.DeleteData(
                 schema: "sec",
@@ -46,11 +48,14 @@ namespace Kharbarchi.Server.Migrations
                 keyColumn: "Id",
                 keyValue: "db563459-d897-400f-87d2-747d86f2b236");
 
-            migrationBuilder.DeleteData(
-                schema: "sec",
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "f517b79d-1fc4-4800-bcb8-ee0ca67dce1e");
+           
+            migrationBuilder.UpdateData(
+                 schema: "sec",
+                 table: "AspNetRoles",
+                 keyColumn: "Id",
+                 keyValue: "6240e185-5c3a-410b-99d3-9767571fdf24",
+                 columns: new[] { "Name", "NormalizedName" },
+                 values: new object[] { "User", "USER" });
         }
     }
 }
